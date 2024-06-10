c5dd8f6bb7480bd92ff1ad475cadd9bb654991f1
junwon-hwnag <nyong1320@gmail.com>
2024-06-10 11:08:26 +0900

    # 카카오 로그인 토큰 및 사용자 정보 가져오기

--- src/main/java/com/study/springstudy/springmvc/chap05/api/SnsLoginController.java
+++ src/main/java/com/study/springstudy/springmvc/chap05/api/SnsLoginController.java
@@ -1,13 +1,19 @@
 package com.study.springstudy.springmvc.chap05.api;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.HashMap;
+
 @Controller
 @Slf4j
+@RequiredArgsConstructor
 public class SnsLoginController {
+
+    private final com.study.springstudy.springmvc.chap05.Service.SnsLoginService snsLoginService;
 
     @Value("${sns.kakao.app-key}")
     private String appKey;
@@ -30,8 +36,20 @@ public class SnsLoginController {
     @GetMapping("/oauth/kakao")
     public String kakaoCode(String code){
         log.info("카카오 인가코드 발급-{}",code);
+
+        // 토큰 발급에 필요한 파라미터 만들기
+        HashMap<Object, Object> requestParams = new HashMap<>();
+        requestParams.put("appKey",appKey);
+        requestParams.put("redirect",redirectUri);
+        requestParams.put("code",code);
+
+        // 인증 액세스 토큰 발급 요청
+        snsLoginService.kakaoLogin(requestParams);
+
         return "";
     }
+
+
 
 
 }
